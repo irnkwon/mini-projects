7916c527846e5805d335fcdf3bce984391205ef9
irnkwon <irene.haeryun.kwon@gmail.com>
2023-01-30 14:27:40 -0500

    add blurry loading

---
+++ blurry-loading/script.js
@@ -0,0 +1,21 @@
+const loadingText = document.querySelector('.loading-text')
+const bg = document.querySelector('.bg')
+
+let percentage = 0
+let interval = setInterval(blurring, 30);
+
+function blurring() {
+    percentage++
+
+    if (percentage > 99) {
+        clearInterval(interval)
+    }
+
+    loadingText.innerText = `${percentage}%`
+    loadingText.style.opacity = scale(percentage, 0, 100, 1, 0)
+    bg.style.filter = `blur(${scale(percentage, 0, 100, 30, 0)}px)`
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+}
